b29c266abc4ce3da7abc1e2c37cbf648492d1842
Peter Christopher Bach <petercb@petercb-linux-001.ad.ife.no>
2025-06-28 23:51:37 +0200

    Added CORS handling to avoid client errors
    Also made the code a bit more readable.

--- server/index.js
+++ server/index.js
@@ -1,9 +1,11 @@
 const express = require('express');
 const sqlite3 = require('sqlite3').verbose();
+const cors = require('cors');
 const app = express();
 const db = new sqlite3.Database(':memory:');
 
 app.use(express.json());
+app.use(cors());
 
 db.serialize(() => {
 
@@ -14,8 +16,11 @@ db.serialize(() => {
 
 app.get('/test', (req, res) => {
 	db.all('SELECT * FROM test', [], (err, rows) => {
-		if(err) return res.status(500).json({error: err.message});
-		res.json(rows);
+		if (err) {
+			return res.status(500).json({error: err.message});
+		}
+
+		return res.json(rows);
 	});
 });
 
