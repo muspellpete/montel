21346fba3b02e44510a7769f53c486cefd23325f
Peter Christopher Bach <petercb@petercb-linux-001.ad.ife.no>
2025-06-28 23:52:34 +0200

    App now displays data from database
    The result is just displayed as a simple string for now.

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,31 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {useEffect, useState} from 'react';
 import './App.css';
 
+type Test = {
+	id: number;
+	name: string;
+}
+
 function App() {
+
+
+	const [test, setTest] = useState<Test[]>([]);
+	useEffect(() => { 
+		fetch('http://localhost:3001/test')
+		.then(response => {
+			console.log(response);
+			return response.json();
+		})
+		.then(json => {
+			setTest(json);
+			console.log(json);
+		})
+		.catch(error => console.log(error));
+	}, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+
+	  <div>From database: {test.map(testEntry => testEntry.name)}</div>
   );
 }
 
