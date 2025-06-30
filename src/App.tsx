664dde9f45e53fbb1a4336ec21674ddaeac305e1
Peter Christopher Bach <petercb@petercb-linux-001.ad.ife.no>
2025-06-30 14:33:11 +0200

    Added basic visualization of cars
    Each car is implicitly not available until this can be verified by
    consulting the booking registry in the database.

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,21 @@
-import React, {useEffect, useState} from 'react';
+import {useEffect, useState} from 'react';
+import {Booking, Car} from '../shared/types';
 import './App.css';
-
-type Test = {
-	id: number;
-	name: string;
-}
 
 function App() {
 
 
-	const [test, setTest] = useState<Test[]>([]);
+	const [cars, setCars] = useState<[Car, boolean][]>([]);
 	useEffect(() => { 
-		fetch('http://localhost:3001/test')
+		
+		fetch('http://localhost:3001/cars')
 		.then(response => {
 			console.log(response);
 			return response.json();
 		})
 		.then(json => {
-			setTest(json);
+			// Default to not being available until we can check
+			setCars(json.map((car: Car) => [car, false] as [Car, boolean]));
 			console.log(json);
 		})
 		.catch(error => console.log(error));
@@ -25,7 +23,18 @@ function App() {
 
   return (
 
-	  <div>From database: {test.map(testEntry => testEntry.name)}</div>
+	  <div><table><tbody>
+	  
+	  {
+		  cars.map(([car, available]) => <tr key={car.id}>
+			  <td>{car.registration}</td>
+			  <td><button disabled={!available}>Book</button></td>
+			  <td>Available: {available ? "yes" : "no"}</td>
+			</tr>)
+	  }
+
+	  </tbody></table></div>
+
   );
 }
 
